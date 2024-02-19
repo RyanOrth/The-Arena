754abe875ed5dec9dcea0742c451737d2116b318
RyanOrth <ryan.orth@live.com>
2024-02-19 15:54:11 -0600

    Added mirror package and MirrorVR example scripts

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void TakeDamage(int amount)
     {
-        
+        // Spawn health bar
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            TakeDamage(Random.Range(10, 25));
+        }
     }
 }
